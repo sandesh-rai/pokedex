45742673787d8eb5493d11436bcdc22eff1e30c1
Sandesh <sandesh.thulung.rai@gmail.com>
2023-01-10 19:32:02 +0000

    Make border radius responsive

--- src/components/PokedexRightPanelScreen.js
+++ src/components/PokedexRightPanelScreen.js
@@ -1,9 +1,6 @@
-import React from 'react'
-import styles from './PokedexRightPanelScreen.module.css'
+import React from "react";
+import styles from "./PokedexRightPanelScreen.module.css";
 
 export default function PokedexRightPanelScreen() {
-  return (
-    <div class={styles.rightPanelScreen}>
-    </div>
-  )
+  return <div className={styles.rightPanelScreen}></div>;
 }
