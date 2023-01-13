fbca0089ac42acaa1430709855d22951280b1e2c
Sandesh <sandesh.thulung.rai@gmail.com>
2023-01-13 16:31:20 +0000

    Install react redux and axios, and make get request to get all pokemon on component mount

--- src/components/Pokedex.js
+++ src/components/Pokedex.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./Pokedex.module.css";
 import PokedexHinge from "./PokedexHinge";
 import LeftPanel from "./LeftPanel";
 import RightPanel from "./RightPanel";
+import axios from "axios";
 
 export default function Pokedex() {
+  const [pokemonList, setPokemonList] = useState([]);
+
+  const getAllPokemon = async () => {
+    const { data } = await axios.get(
+      `https://pokeapi.co/api/v2/pokemon?limit=1000`
+    );
+    setPokemonList(data);
+  };
+
+  useEffect(() => {
+    getAllPokemon();
+  }, []);
+
   return (
     <div className={styles.container_outer}>
       <div className={styles.panels}>
