dfd6316b4567b924632affa8e4edd11e405bd82b
Sandesh <sandesh.thulung.rai@gmail.com>
2023-01-14 17:55:54 +0000

    Show selected pokemon name on right panel screen

--- src/components/RightPanelScreen.js
+++ src/components/RightPanelScreen.js
@@ -1,6 +1,20 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import styles from "./RightPanelScreen.module.css";
 
 export default function RightPanelScreen() {
-  return <div className={styles.rightPanelScreen}></div>;
+  const pokemonName = useSelector(
+    (state) => state.pokemon.selectedPokemon.name
+  );
+
+  function formatName(string) {
+    if (!string) return "";
+    return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+  }
+
+  return (
+    <div className={styles.rightPanelScreen}>
+      <h2>{formatName(pokemonName)}</h2>
+    </div>
+  );
 }
