27b3a71ec69efb3bac54fd9bd76c820c3edf84b2
sandesh-rai <sandesh.thulung.rai@gmail.com>
2023-02-07 23:34:34 +0000

    Convert pokemonSlice to ts

--- src/features/pokemon/pokemonSlice.ts
+++ src/features/pokemon/pokemonSlice.ts
@@ -1,11 +1,20 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { AllPokemon, SelectedPokemon } from "../../types";
+
+// Define a type for the slice state
+interface PokemonState {
+  allPokemon: AllPokemon;
+  selectedPokemon: SelectedPokemon;
+}
+
+const initialState: PokemonState = {
+  allPokemon: [] as AllPokemon,
+  selectedPokemon: {} as SelectedPokemon,
+};
 
 export const pokemonSlice = createSlice({
   name: "pokemon",
-  initialState: {
-    allPokemon: [],
-    selectedPokemon: {},
-  },
+  initialState,
   reducers: {
     setAllPokemon: (state, action) => {
       state.allPokemon = [...action.payload];
